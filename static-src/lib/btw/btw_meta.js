3b2e3c023f8fccf56073bc069dd75b6b9c38b046
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-07-03 14:18:00 -0400

    Initial support for meta-information.

---
+++ static-src/lib/btw/btw_meta.js
@@ -0,0 +1,42 @@
+define(function (require, exports, module) {
+'use strict';
+
+var $ = require("jquery");
+var oop = require("wed/oop");
+var util = require("wed/util");
+var TEIMeta = require("wed/modes/generic/metas/tei_meta").Meta;
+
+/**
+ * @class
+ */
+function BTWMeta() {
+    TEIMeta.call(this);
+}
+
+oop.inherit(BTWMeta, TEIMeta);
+
+BTWMeta.prototype.isInline = function (node) {
+    var $node = $(node);
+    // We need to normalize the name to fit the names we have below.
+    var original_name = util.getOriginalName(node);
+    var parts = original_name.split(":");
+    // XXX this is taking a shortcut. We should instead find the
+    // namespace of the node and convert it to an appropriate prefix
+    // to use below.
+    if (parts.length === 1) {
+        parts[1] = parts[0];
+        parts[0] = "tei";
+    }
+    var name = parts.join(":");
+
+    switch(name) {
+    case "btw:sf":
+        return true;
+    default:
+        return TEIMeta.prototype.isInline.call(this, node);
+    }
+};
+
+exports.Meta = BTWMeta;
+
+});
