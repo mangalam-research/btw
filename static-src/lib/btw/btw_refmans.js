780382de66c4c37ae2e7ab73cac3b58135796005
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-07-04 07:28:39 -0400

    Make allocateLabel return the same label if already allocated.

--- static-src/lib/btw/btw_refmans.js
+++ static-src/lib/btw/btw_refmans.js
@@ -14,16 +14,18 @@ function SenseReferenceManager() {
 oop.inherit(SenseReferenceManager, ReferenceManager);
 
 SenseReferenceManager.prototype.allocateLabel = function (id) {
-    // More than 26 senses in a single article seems much.
-    if (this._next_sense_label_ix >= sense_labels.length)
-        throw new Error("hit the hard limit of 26 sense labels in a " +
-                        "single article");
+    var label = this._id_to_label[id];
+    if (label === undefined) {
+        // More than 26 senses in a single article seems much.
+        if (this._next_sense_label_ix >= sense_labels.length)
+            throw new Error("hit the hard limit of 26 sense labels in a " +
+                            "single article");
 
-    /* jshint boss:true */
-    var label = this._id_to_label[id] =
-        sense_labels[this._next_sense_label_ix++];
+        label = this._id_to_label[id] =
+            sense_labels[this._next_sense_label_ix++];
 
-    this._subsense_reference_managers[id] = new SubsenseReferenceManager(label);
+        this._subsense_reference_managers[id] = new SubsenseReferenceManager(label);
+    }
 
     return label;
 };
@@ -52,8 +54,11 @@ function SubsenseReferenceManager(parent_label) {
 oop.inherit(SubsenseReferenceManager, ReferenceManager);
 
 SubsenseReferenceManager.prototype.allocateLabel = function (id) {
-    /* jshint boss:true */
-    return this._id_to_label[id] = this._next_label++;
+    var label = this._id_to_label[id];
+    if (label === undefined)
+        label = this._id_to_label[id] = this._next_label++;
+    console.log(id, label);
+    return label;
 };
 
 SubsenseReferenceManager.prototype.idToLabel = function (id) {
