bf439d1f048b9946ce6049a2bfa482aaba8d112d
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-07-03 21:41:38 -0400

    Added the refman for subsenses.

--- static-src/lib/btw/btw_refmans.js
+++ static-src/lib/btw/btw_refmans.js
@@ -7,29 +7,66 @@ var ReferenceManager = require("wed/refman").ReferenceManager;
 var sense_labels = 'abcdefghijklmnopqrstuvwxyz';
 function SenseReferenceManager() {
     ReferenceManager.call(this, "sense");
+    this._subsense_reference_managers = {};
     this._next_sense_label_ix = 0;
 }
 
 oop.inherit(SenseReferenceManager, ReferenceManager);
 
-(function () {
-    this.allocateLabel = function (id) {
-        // More than 26 senses in a single article seems much.
-        if (this._next_sense_label_ix >= sense_labels.length)
-            throw new Error("hit the hard limit of 26 sense labels in a single article");
+SenseReferenceManager.prototype.allocateLabel = function (id) {
+    // More than 26 senses in a single article seems much.
+    if (this._next_sense_label_ix >= sense_labels.length)
+        throw new Error("hit the hard limit of 26 sense labels in a " +
+                        "single article");
 
-        /* jshint boss:true */
-        return this._id_to_label[id] = sense_labels[this._next_sense_label_ix++];
-    };
+    /* jshint boss:true */
+    var label = this._id_to_label[id] =
+        sense_labels[this._next_sense_label_ix++];
 
-    this.idToLabelForHead = function (id) {
-        return this.idToLabel(id).toUpperCase();
-    };
+    this._subsense_reference_managers[id] = new SubsenseReferenceManager(label);
 
-    this._deallocateAllLabels = function () {
-        this._next_sense_label_ix = 0;
-    };
-}).call(SenseReferenceManager.prototype);
+    return label;
+};
+
+SenseReferenceManager.prototype.idToLabelForHead = function (id) {
+    return this.idToLabel(id).toUpperCase();
+};
+
+SenseReferenceManager.prototype.idToSubsenseRefman = function (id) {
+    return this._subsense_reference_managers[id];
+};
+
+SenseReferenceManager.prototype._deallocateAllLabels = function () {
+    this._next_sense_label_ix = 0;
+    this._subsense_reference_managers = {};
+};
 
 exports.SenseReferenceManager = SenseReferenceManager;
+
+function SubsenseReferenceManager(parent_label) {
+    ReferenceManager.call(this, "subsense");
+    this._next_label = 1;
+    this._parent_label = parent_label;
+}
+
+oop.inherit(SubsenseReferenceManager, ReferenceManager);
+
+SubsenseReferenceManager.prototype.allocateLabel = function (id) {
+    /* jshint boss:true */
+    return this._id_to_label[id] = this._next_label++;
+};
+
+SubsenseReferenceManager.prototype.idToLabel = function (id) {
+    return this._parent_label +
+        ReferenceManager.prototype.idToLabel.call(this, id);
+};
+
+SubsenseReferenceManager.prototype.idToLabelForHead =
+        SubsenseReferenceManager.prototype.idToLabel;
+
+
+SubsenseReferenceManager.prototype._deallocateAllLabels = function () {
+    this._next_label = 1;
+};
+
 });
