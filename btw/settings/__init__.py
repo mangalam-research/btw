7fcf576e20ded5cf8d68d5dd443c6dba3ea4a476
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-08-09 10:36:33 -0400

    We need the transaction middleware.

--- btw/settings/__init__.py
+++ btw/settings/__init__.py
@@ -127,6 +127,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
 )
 
 ROOT_URLCONF = 'btw.urls'
