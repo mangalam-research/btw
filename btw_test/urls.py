0f6bdac8390ec48bb86c4ea97d62ff44b94a755c
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-08-25 20:38:15 -0400

    Take care of pylint warnings and Django deprecation warnings.

--- btw_test/urls.py
+++ btw_test/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url, patterns
 from django.views.generic import TemplateView
 
 urlpatterns = patterns(
     '',
+    # Pylint generates a false positive on the next line of code.
+    # pylint: disable=E1120
     url(r'^$', TemplateView.as_view(template_name="btw_test/test.html")),
     )
