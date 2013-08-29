cdd35edb79f39ccb2b04093945586283c7ec35a3
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-08-28 21:55:34 -0400

    Code cleanup and deal with the csrftoken.

--- bibsearch/static/scripts/modules/bibSearch.js
+++ bibsearch/static/scripts/modules/bibSearch.js
@@ -1,93 +1,86 @@
-define(["jquery"], function (jQuery) {
+define(["jquery", "jquery.cookie"], function ($) {
 
-    var bibSearch = function () {
-        // populate html by iterating the given data object
-        function populateHTML(data) {
-            // html population steps
-            jQuery("#result_list").html(data);
-        }
+var bibSearch = function () {
 
-        function ajaxResult(pageNumber) {
-            var dataString = "page=" + pageNumber
-            jQuery.ajax({
-                url: '/search/results/',
-                context: document.body,
-                data: dataString
-            })
-                .done(function (data) {
-                populateHTML(data);
-                // add jquery selector for the new sync buttons.
-                jQuery("#result_list input").click(function () {
-                    ajaxSync(this.name);
-                });
+    var csrftoken = $.cookie('csrftoken');
 
-                jQuery("#result_list a").click(function () {
-                    ajaxResult(this.name);
-                });
-            });
-        }
+    var headers =  {
+        'X-CSRFToken': csrftoken
+    };
 
-        function ajaxSync(encString) {
-            var dataString = "enc=" + encString
-            jQuery.ajax({
-                url: '/search/sync/',
-                context: document.body,
-                data: dataString,
-                beforeSend: function () {
-                    jQuery('#loadingDiv').css('display', 'inline');
-                    return true;
-                }
-            })
-                .done(function (data) {
-                // remove the button which was clicked
-                // to sync information
-                var parentObject = jQuery("[" + "name='" +
-                    encString + "']").parent();
-                parentObject.html("NA");
-                if (data == "OK") {
-                    alert("Copy successful!.");
-                } else if (data == "DUP") {
-                    alert("Duplicate item not copied.")
-                } else {
-                    alert(data);
-                }
-            })
-                .always(function () {
-                jQuery('#loadingDiv').css('display', 'none');
-            });
-        }
+    function refreshResults(data) {
+        var $result_list = $("#result_list");
+        $result_list.html(data);
 
-        // create a ajax search call
-        this.ajaxSearch = function () {
-            var dataString = "library=" + jQuery('#id_library').val() + "&keyword=" +
-                jQuery('#id_keyword').val();
-            jQuery.ajax({
-                url: '/search/',
-                context: document.body,
-                data: dataString,
-                beforeSend: function () {
-                    jQuery('#loadingDiv').css('display', 'inline');
-                    return true;
-                }
-            })
-                .done(function (data) {
-                populateHTML(data);
-                // add jquery selector for the new sync buttons.
-                jQuery("#result_list input").click(function () {
-                    ajaxSync(this.name);
-                });
+        // add jquery selector for the new sync buttons.
+        $result_list.find(".bibsearch-copy").click(function () {
+            ajaxSync(this.name);
+        });
 
-                jQuery("#result_list a").click(function () {
-                    ajaxResult(this.name);
-                });
-            })
-                .always(function () {
-                jQuery('#loadingDiv').css('display', 'none');
-            });
-        }
+        $result_list.find("a").click(function () {
+            ajaxResult(this.name);
+        });
 
+        $result_list.trigger("bibsearch-refresh-results");
     }
 
-    return bibSearch;
+    function ajaxResult(pageNumber) {
+        var dataString = "page=" + pageNumber;
+        $.ajax({
+            url: '/search/results/',
+            data: dataString,
+            headers: headers
+        }).done(refreshResults);
+    }
+
+    function ajaxSync(encString) {
+        var dataString = "enc=" + encString;
+        $.ajax({
+            type: "POST",
+            url: '/search/sync/',
+            data: dataString,
+            headers: headers,
+            beforeSend: function () {
+                $('#loading').css('display', 'inline');
+                return true;
+            }
+        }).done(function (data) {
+            // remove the button which was clicked
+            // to sync information
+            var parentObject = $("[" + "name='" +
+                                 encString + "']").parent();
+            parentObject.html("NA");
+            if (data == "OK") {
+                alert("Copy successful!.");
+            } else if (data == "DUP") {
+                alert("Duplicate item not copied.");
+            } else {
+                alert(data);
+            }
+        }).always(function () {
+            $('#loading').css('display', 'none');
+        });
+    }
+
+    // create a ajax search call
+    this.ajaxSearch = function () {
+        var dataString = "library=" + $('#id_library').val() + "&keyword=" +
+                $('#id_keyword').val();
+        $.ajax({
+            type: "POST",
+            url: '/search/exec/',
+            data: dataString,
+            headers: headers,
+            beforeSend: function () {
+                $('#loading').css('display', 'inline');
+                return true;
+            }
+        }).done(refreshResults).always(function () {
+            $('#loading').css('display', 'none');
+        });
+    };
+};
+
+return bibSearch;
 
 });
