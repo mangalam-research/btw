3806c1ee5b7a27cbf032f4066099cd4f91695493
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-09-11 15:46:14 -0400

    Use longer names for url names.

--- lexicography/admin.py
+++ lexicography/admin.py
@@ -1,13 +1,16 @@
 from django.utils.html import escape, mark_safe
 from django.core import urlresolvers
 from django.contrib import admin
-from .models import Entry, Chunk, ChangeRecord, UserAuthority, OtherAuthority, Authority, EntryLock
+from .models import Entry, Chunk, ChangeRecord, UserAuthority, \
+    OtherAuthority, Authority, EntryLock
 
 from btw import settings
+
 
 def make_link_method(field_name, display_name=None):
     if display_name is None:
         display_name = field_name
+
     def method(self, obj):
         field = getattr(obj, field_name)
         # pylint: disable-msg=W0212
@@ -17,19 +20,22 @@ def make_link_method(field_name, display_name=None):
                                                model_name + "_change",
                                                args=(field, )),
                           escape(str(field))))
-    method.allow_tags=True
-    method.short_description=display_name
+    method.allow_tags = True
+    method.short_description = display_name
     return method
 
+
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ('headword', 'user', 'datetime', 'session', 'ctype', 'csubtype', 'raw_edit', 'chunk_link')
+    list_display = ('headword', 'user', 'datetime', 'session', 'ctype',
+                    'csubtype', 'raw_edit', 'chunk_link')
 
     chunk_link = make_link_method('c_hash', "Chunk")
 
     def raw_edit(self, obj):
         return mark_safe('<a href="%s">Edit raw XML</a>' %
-                         (urlresolvers.reverse('entry_rawupdate',
+                         (urlresolvers.reverse('lexicography_entry_rawupdate',
                                                args=(obj.id, ))))
+
 
 class ChangeRecordAdmin(admin.ModelAdmin):
     class Media(object):
@@ -37,14 +43,17 @@ class ChangeRecordAdmin(admin.ModelAdmin):
               settings.BTW_REQUIREJS_CONFIG_PATH,
               '/'.join([settings.STATIC_URL, 'js/lexicography/admin.js']))
 
-    list_display = ('entry', 'headword', 'user', 'datetime', 'session', 'ctype', 'csubtype', 'revert', 'chunk_link')
+    list_display = ('entry', 'headword', 'user', 'datetime', 'session',
+                    'ctype', 'csubtype', 'revert', 'chunk_link')
 
     chunk_link = make_link_method('c_hash', "Chunk")
 
     def revert(self, obj):
-        return mark_safe('<a class="lexicography-revert" href="%s">Revert entry to this version</a>' %
-                         (urlresolvers.reverse('change_revert',
+        return mark_safe(('<a class="lexicography-revert" href="%s">'
+                          'Revert entry to this version</a>') %
+                         (urlresolvers.reverse('lexicography_change_revert',
                                                args=(obj.id, ))))
+
 
 class EntryLockAdmin(admin.ModelAdmin):
     list_display = ('entry', 'owner', 'datetime')
--- lexicography/urls.py
+++ lexicography/urls.py
@@ -3,26 +3,28 @@ from btw import settings
 
 urlpatterns = patterns(
     'lexicography.views',
-    url(r'^$', 'main', name='main'),
+    url(r'^$', 'main', name='lexicography_main'),
     url(r'^search$', 'search'),
     url(r'^collect$', 'collect'),
-    url(r'^entry/(?P<entry_id>\d+)/$', 'entry_details', name='entry_details'),
+    url(r'^entry/(?P<entry_id>\d+)/$', 'entry_details',
+        name='lexicography_entry_details'),
     url(r'^entry/(?P<entry_id>\d+)/update$', 'entry_update',
-        name="entry_update"),
-    url(r'^handle/(?P<handle>.+)/save$', 'handle_save', name='handle_save'),
+        name="lexicography_entry_update"),
+    url(r'^handle/(?P<handle>.+)/save$', 'handle_save',
+        name='lexicography_handle_save'),
     url(r'^entry/(?P<entry_id>\d+)/raw_update$', 'entry_raw_update',
-        name="entry_rawupdate"),
+        name="lexicography_entry_rawupdate"),
     url(r'^change/(?P<change_id>\d+)/revert$', 'change_revert',
-        name="change_revert"),
+        name="lexicography_change_revert"),
 
-    url(r'^entry/new$', 'entry_new', name='entry_new'),
+    url(r'^entry/new$', 'entry_new', name='lexicography_entry_new'),
     #LoginRequiredCreateView.as_view(form_class=SaveForm,
     # template_name="lexicography/new.html"),
-    url(r'^log$', 'log', name='log'),
-    )
+    url(r'^log$', 'log', name='lexicography_log'),
+)
 
 if settings.DEBUG:
     urlpatterns += patterns(
         'lexicography.views',
         url(r'^editing_data$', 'editing_data'),
-        )
+    )
