78e73d5bfbe99cb9d199d49febf14b85bf762b5e
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-09-11 15:15:11 -0400

    Moved util into lib. Added utcnow. Cleaned module up.

--- lib/util.py
+++ lib/util.py
@@ -1,13 +1,19 @@
+from django.db.models import Q
+from django.utils.timezone import utc
+
 import re
 import operator
-from django.db.models import Q
+import datetime
 import tempfile
 import os
 import subprocess
 
+
 class WithTmpFiles(object):
     def __init__(self, input_data=None):
         self._input_data = input_data
+        self._tmpinput_path = None
+        self._tmpoutput_path = None
 
     def __enter__(self):
         (tmpinput_file, tmpinput_path) = tempfile.mkstemp(prefix='btwtmp')
@@ -26,19 +32,21 @@ class WithTmpFiles(object):
         os.unlink(self._tmpinput_path)
         os.unlink(self._tmpoutput_path)
 
+
 def run_saxon(xsl_path, input_data):
-    with WithTmpFiles(input_data) as (tmpinput_file, tmpinput_path,
-                                         tmpoutput_file, tmpoutput_path):
-        subprocess.check_call(["saxon", "-s:" + tmpinput_path, "-xsl:" + xsl_path,
-                               "-o:" + tmpoutput_path])
+    with WithTmpFiles(input_data) as (_, tmpinput_path,
+                                      tmpoutput_file, tmpoutput_path):
+        subprocess.check_call(["saxon", "-s:" + tmpinput_path, "-xsl:" +
+                               xsl_path, "-o:" + tmpoutput_path])
         out = os.fdopen(tmpoutput_file, 'r')
         ret = out.read().decode('utf-8')
         out.close()
         return ret
 
+
 def run_xsltproc(xsl_path, input_data):
-    with WithTmpFiles(input_data) as (tmpinput_file, tmpinput_path,
-                                         tmpoutput_file, tmpoutput_path):
+    with WithTmpFiles(input_data) as (_, tmpinput_path,
+                                      tmpoutput_file, tmpoutput_path):
         subprocess.check_call(["xsltproc", "-o", tmpoutput_path, xsl_path,
                                tmpinput_path])
 
@@ -48,25 +56,37 @@ def run_xsltproc(xsl_path, input_data):
         return ret
 
 
+def utcnow():
+    """
+:returns: The date and time now with the timezone information set
+          to UTC. This avoids warnings about naive times.
+
+:rtype: :class:`datetime.datetime`
+"""
+    return datetime.datetime.utcnow().replace(tzinfo=utc)
+
+
 # normalize_query and get_query adapted from code on Julien Phalip's
 # site.
 def normalize_query(query_string,
                     findterms=re.compile(r'"(?:[^"]+)"|(?:\S+)').findall,
                     normspace=re.compile(r'\s{2,}').sub):
-    ''' Splits the query string in invidual keywords, getting rid of
-    unecessary spaces and grouping quoted words together.  Example:
-
-    >>> normalize_query('  some random  words "with   quotes  " and   spaces')
-    ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
-
     '''
+Splits the query string in invidual keywords, getting rid of
+unecessary spaces and grouping quoted words together.  Example:
+
+>>> normalize_query('  some random  words "with   quotes  " and   spaces')
+['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+'''
     return [normspace(' ', t.strip()) for t in findterms(query_string)]
 
+
 def get_query(query_string, search_fields):
-    ''' Returns a query, that is a combination of Q objects. That
-        combination aims to search keywords within a model by testing
-        the given search fields.
     '''
+Returns a query, that is a combination of Q objects. That
+combination aims to search keywords within a model by testing the
+given search fields.
+'''
     terms = normalize_query(query_string)
     # What this does is create a field_icontains query for each field.
     # Each of these queries are ORed together so that a given term
