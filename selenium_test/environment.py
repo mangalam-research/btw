bb1f5dd72cf53e3bcbc0048e1d107588048b7324
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-10-15 11:39:29 -0400

    Modified the selenium test setup to use selenic.

--- selenium_test/environment.py
+++ selenium_test/environment.py
@@ -2,18 +2,27 @@ import os
 
 _dirname = os.path.dirname(__file__)
 
+local_conf_path = os.path.join(os.path.dirname(_dirname),
+                               "build", "config", "selenium_local_config.py")
+
+
 conf_path = os.path.join(os.path.dirname(_dirname),
                          "build", "config", "selenium_test_config.py")
 conf = {"__file__": conf_path}
 execfile(conf_path, conf)
 
+config = conf["Config"](local_conf_path)
+
+import selenic.util
+
 
 def before_all(context):
-    context.driver = conf["get_driver"]()
+    context.driver = config.get_driver()
+    context.util = selenic.util.Util(context.driver)
 
 
 def after_all(context):
     driver = context.driver
-    conf["set_test_status"](driver.session_id, not context.failed)
-    if not context.failed:
+    config.set_test_status(driver.session_id, not context.failed)
+    if not context.failed and "BEHAVE_NO_QUIT" not in os.environ:
         driver.quit()
