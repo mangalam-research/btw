be70ba9a79fc89ab53be48f546e1d48a815944f7
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-11-04 10:40:58 -0500

    Build change to keep in line with salve.

--- utils/schemas/include.mk
+++ utils/schemas/include.mk
@@ -2,18 +2,13 @@ define UTIL_SCHEMAS_TEMPLATE
 $(1)_include_mk_DIR=$(2)
 $(1)_outdir=$$($(1)_include_mk_DIR)/out
 
-$(1)_SALVE_ROOT=~/src/git-repos/salve/
-$(1)_SIMPLIFY=salve-simplify
-$(1)_RNG_TO_JS=$$($(1)_SALVE_ROOT)/lib/salve/rng-to-js.xsl
+$(1)_CONVERT=salve-convert
 
 $$($(1)_outdir)/btw-storage.rng: $$($(1)_include_mk_DIR)/btw-storage.xml
 	roma --xsl=/usr/share/xml/tei/stylesheet --noxsd --nodtd $$< $$($(1)_outdir)
 
-$$($(1)_outdir)/btw-storage-simplified.rng: $$($(1)_outdir)/btw-storage.rng
-	$$($(1)_SIMPLIFY) $$< $$@
-
-$$($(1)_outdir)/btw-storage.js: $$($(1)_outdir)/btw-storage-simplified.rng
-	xsltproc $$($(1)_RNG_TO_JS) $$< > $$@
+$$($(1)_outdir)/btw-storage.js: $$($(1)_outdir)/btw-storage.rng
+	$$($(1)_CONVERT) $$($(1)_RNG_TO_JS) $$< $$@
 
 clean:
 	rm -rf $$($(1)_outdir)
