b8d6bffc8416fa0228cf37b50f159cb122692d33
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-01-14 13:44:10 -0500

    Drop the semi colon check; the pep8 check takes care of it.

--- .glerbl/repo_conf.py
+++ .glerbl/repo_conf.py
@@ -1,5 +1,4 @@
 checks = {
     "pre-commit": ["no_before_commit", "no_non_ascii_filenames",
-                   "no_trailing_whitespace", "python_no_trailing_semicolon",
-                   "python_pep8"]
+                   "no_trailing_whitespace", "python_pep8"]
 }
