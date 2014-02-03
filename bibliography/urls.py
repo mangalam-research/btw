0b1950f6928fe5218278845a3d6239a8807368ee
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-02-03 14:59:17 -0500

    Reorganized the templates to provide some sort of site-wide navigation.

--- bibliography/urls.py
+++ bibliography/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, url
 from django.conf import settings
 
 urlpatterns = patterns('bibliography.views',
-                       url(r'^search/$', 'search', name='search'),
-                       url(r'^exec/$', 'exec_', name='exec'),
-                       url(r'^results/$', 'results', name='results'),
+                       url(r'^search/$', 'search', name='bibliography_search'),
+                       url(r'^exec/$', 'exec_', name='bibliography_exec'),
+                       url(r'^results/$', 'results',
+                           name='bibliography_results'),
                        url(r'^(?P<itemKey>.+?)/abbrev/$', 'abbrev',
-                           name='abbrev'),
-                       url(r'^(?P<itemKey>.+?)/info/$', 'info', name='info'),
+                           name='bibliography_abbrev'),
+                       url(r'^(?P<itemKey>.+?)/info/$', 'info',
+                           name='bibliography_info'),
                        url(r'^sync/$', 'sync', name='sync'),)
 
 # tests views are only made available during development
