508ba52360ea0cad3df81d45417a4f0e5e0aa467
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-02-21 09:22:44 -0500

    Added a main page.

--- btw/urls.py
+++ btw/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^$', TemplateView.as_view(template_name='main.html'), name="main"),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'allauth.account.views.login', name="login"),
