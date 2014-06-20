61b671be20a85ab302b9c2244e7e892e62319441
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-06-20 16:16:37 -0400

    Made the toolbar internals more sensible.
    Removed some old code that was no longer used. And now use the
    attribute ``name`` rather than ``id`` to identify the buttons, which
    helps in the test suite.

--- static-src/lib/btw/btw_toolbar.js
+++ static-src/lib/btw/btw_toolbar.js
@@ -11,37 +11,39 @@ var oop = require("wed/oop");
 function Toolbar(editor) {
     this._editor = editor;
     this._buttons = [
-        new UndoAction(editor),
-        new RedoAction(editor),
-        new btw_tr.RemoveMixedTr(editor),
-        new btw_tr.SetTextLanguageTr(editor, "Sanskrit"),
-        new btw_tr.SetTextLanguageTr(editor, "Pāli"),
-        new btw_tr.SetTextLanguageTr(editor, 'Latin'),
+        {name: "quit", action: new QuitAction(editor)},
+        {name: "save", action: new SaveAction(editor)},
+        {name: "undo", action: new UndoAction(editor)},
+        {name: "redo", action: new RedoAction(editor)},
+        {name: "remove", action: new btw_tr.RemoveMixedTr(editor)},
+        {name: "sanskrit",
+         action: new btw_tr.SetTextLanguageTr(editor, "Sanskrit")},
+        {name: "pali",
+         action: new btw_tr.SetTextLanguageTr(editor, "Pāli")},
+        {name: "latin",
+         action: new btw_tr.SetTextLanguageTr(editor, 'Latin')},
     ];
+
+    this._name_to_action = Object.create(null);
 
     this.$top = $('<div id="toolbar" class="ui-widget-header ui-corner-all">');
     for(var button_ix = 0, button;
         (button = this._buttons[button_ix]) !== undefined; button_ix++) {
-        var name = button;
+        var name = button.name;
+        var action = button.action;
         var $button;
-        if (name instanceof $) {
-            $button = name;
-            $button.attr('id', button_ix);
-        }
-        else if (name instanceof action.Action) {
-            var icon = name.getIcon();
-            $button = $("<button class='btn btn-default' id='" + button_ix + "'>" +
-                        (icon || name.getAbbreviatedDescription()) + "</button>");
-            if (icon || name.getAbbreviatedDescription() !==
-                name.getDescription())
-                $button.tooltip({title: name.getDescription(),
-                                container: "body"});
-        }
-        else
-            $button = $("<button class='btn btn-default' id='" + button_ix +
-                        "'>" + name + "</button>");
+        var icon = action.getIcon();
+        $button = $("<button class='btn btn-default' name='" + name + "'>" +
+                    (icon || action.getAbbreviatedDescription()) +
+                    "</button>");
+        if (icon || action.getAbbreviatedDescription() !==
+            action.getDescription())
+            $button.tooltip({title: action.getDescription(),
+                             container: "body",
+                             placement: "auto"});
         $button.click(this._click.bind(this));
 	this.$top.append($button);
+        this._name_to_action[name] = action;
     }
 }
 
@@ -54,15 +56,15 @@ Toolbar.prototype._click = log.wrap(function (ev) {
     ev.stopImmediatePropagation();
     ev.preventDefault();
     var range = this._editor.getSelectionRange();
-    var button_ix = $(ev.delegateTarget).attr("id");
-    var button = this._buttons[button_ix];
-    if (button instanceof btw_tr.SetTextLanguageTr) {
+    var name = $(ev.delegateTarget).attr("name");
+    var act = this._name_to_action[name];
+    if (act instanceof btw_tr.SetTextLanguageTr) {
         // Don't execute if there is no range.
         if (range && !range.collapsed)
-            button.execute();
+            act.execute();
     }
-    else if (button instanceof action.Action)
-        button.execute();
+    else if (act instanceof action.Action)
+        act.execute();
     else
         throw new Error("broken toolbar");
     return false;
@@ -88,6 +90,33 @@ oop.inherit(RedoAction, action.Action);
 
 RedoAction.prototype.execute = function (data) {
     this._editor.redo();
+};
+
+
+function QuitAction(editor) {
+    action.Action.call(this, editor, "Save and quit", "Save and quit",
+                       "<i class='icon-ban-circle' style='color: red'></i>");
+}
+
+oop.inherit(QuitAction, action.Action);
+
+QuitAction.prototype.execute = function (data) {
+    var $form = this._editor.$gui_root.parents("form").first();
+    this._editor.save(function (err) {
+        if (!err)
+            $form.submit();
+    });
+};
+
+function SaveAction(editor) {
+    action.Action.call(this, editor, "Save", "Save",
+                       "<i class='icon-cloud-upload'></i>");
+}
+
+oop.inherit(SaveAction, action.Action);
+
+SaveAction.prototype.execute = function (data) {
+    this._editor.save();
 };
 
 
