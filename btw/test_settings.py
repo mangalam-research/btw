c90c589a134872be8cdd9b87c0d9604ff3d99ffa
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-06-26 09:07:54 -0400

    Modified so that we can be verbose if needed.

--- btw/test_settings.py
+++ btw/test_settings.py
@@ -1,7 +1,16 @@
 # pylint: disable=W0401,W0614
 from .settings import *
 
+__SILENT = True
+
 LOGGING['loggers'] = {}
-LOGGING['disable_existing_loggers'] = True
+if __SILENT:
+    LOGGING['disable_existing_loggers'] = True
+else:
+    LOGGING['loggers'][''] = {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': True,
+    }
 
 BTW_TESTING = True
