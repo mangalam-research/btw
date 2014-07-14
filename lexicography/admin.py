fc41854d5b78e2df1b961b15304bc693441c1ccc
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-07-14 19:34:34 -0400

    Added a way to add new raw articles.

--- lexicography/admin.py
+++ lexicography/admin.py
@@ -2,8 +2,18 @@ from django.conf import settings
 from django.utils.html import escape, mark_safe
 from django.core import urlresolvers
 from django.contrib import admin
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+
+from .locking import release_entry_lock, entry_lock_required
+from .forms import RawSaveForm
 from .models import Entry, Chunk, ChangeRecord, UserAuthority, \
     OtherAuthority, Authority, EntryLock, Handle
+from .xml import storage_to_editable, XMLTree
+from .views import try_updating_entry
 
 
 def make_link_method(field_name, display_name=None):
@@ -34,6 +44,40 @@ class EntryAdmin(admin.ModelAdmin):
         return mark_safe('<a href="%s">Edit raw XML</a>' %
                          (urlresolvers.reverse('lexicography_entry_rawupdate',
                                                args=(obj.id, ))))
+
+    def get_urls(self):
+        return [
+            url(r'^add_raw/$', self.entry_raw_new,
+                name='admin_lexicography_entry_rawnew'),
+        ] + \
+            super(EntryAdmin, self).get_urls()
+
+    @method_decorator(login_required)
+    @method_decorator(require_http_methods(["GET", "POST"]))
+    def entry_raw_new(self, request):
+        if request.method == 'POST':
+            form = RawSaveForm(request.POST)
+            if form.is_valid():
+                chunk = form.save(commit=False)
+
+                # If it was not already entered in the editable format, then we
+                # need to convert it.
+                if not form.cleaned_data['editable_format']:
+                    chunk.data = storage_to_editable(chunk.data)
+                xmltree = XMLTree(chunk.data)
+
+                entry = Entry()
+                try_updating_entry(request, entry, chunk, xmltree,
+                                   Entry.CREATE, Entry.MANUAL)
+                release_entry_lock(entry, request.user)
+        else:
+            form = RawSaveForm()
+
+        ret = render(request, 'lexicography/raw.html', {
+            'page_title': settings.BTW_SITE_NAME + " | Lexicography | New ",
+            'form': form,
+        })
+        return ret
 
 
 class ChangeRecordAdmin(admin.ModelAdmin):
