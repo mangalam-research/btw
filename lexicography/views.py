4312efcdcd3fe1e897f7cbc911560d00508f31ce
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-10-24 16:30:59 -0400

    Require version 0.19.1 of wed.

--- lexicography/views.py
+++ lexicography/views.py
@@ -337,7 +337,7 @@ def handle_update(request, handle_or_entry_id):
 # not something which someone installing BTW should have the
 # opportunity to change. The Django **code** depends on wed being at a
 # certain version. Changing this is a recipe for disaster.
-REQUIRED_WED_VERSION = "0.18.1"
+REQUIRED_WED_VERSION = "0.19.1"
 
 
 def version_check(version):
