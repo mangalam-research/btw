1b3634d4e9c02c81a38cea97af49fb8cfa46c0d1
Louis-Dominique Dubeau <ldd@lddubeau.com>
2014-11-28 19:22:50 -0500

    Modification for Buildbot.

--- btw/test_settings.py
+++ btw/test_settings.py
@@ -22,8 +22,12 @@ else:
 
 # We use the environment variable JENKINS_HOME to detect whether we
 # are runnning in a Jenkins environment. (Seems safer than some of the
-# other environment variables that Jenkins exports.)
-if os.environ.get('JENKINS_HOME', None):
+# other environment variables that Jenkins exports.) We use 'BUILDBOT'
+# to know whether we are operating in a buildbot environment.
+build_env = os.environ.get('JENKINS_HOME', None) or \
+    os.environ.get('BUILDBOT', None)
+
+if build_env:
     build_id = os.environ['BUILD_TAG']
     for db in DATABASES.itervalues():
         db['TEST_NAME'] = 'test_' + db['NAME'] + '_' + build_id
