0f255cafd536ad8376c47680a7d76f62e34ac2fb
Louis-Dominique Dubeau <ldd@lddubeau.com>
2015-08-27 16:28:25 -0400

    Celery workers use the test database in testing.
    The database configuration does not convey from the main test process to
    celery workers that are started from that process. This change makes it
    so that the workers replicate the test setup of the main process.

--- btw/celery.py
+++ btw/celery.py
@@ -16,7 +16,7 @@ if settings.BTW_SELENIUM_TESTS:
     from core.tests.common_zotero_patch import patch
     patch.start()
 
-from celery.signals import after_setup_logger
+from celery.signals import after_setup_logger, worker_init
 @after_setup_logger.connect
 def configure_logging(sender=None, **kwargs):
     LOGGING = {
@@ -43,6 +43,23 @@ def configure_logging(sender=None, **kwargs):
     # Yes, we cheat and access a private variable. There is no simple
     # way to do this.
     logger.addHandler(logging._handlers['mail_admins'])
+
+
+@worker_init.connect
+def handle_worker_init(sender=None, **kwargs):
+    #
+    # We have to perform here what the test runner does for the main
+    # process.
+    #
+    # The thing is though that we need to do this only if we are *not*
+    # running the Selenium tests because if we *are* then the
+    # modification is done in test_settings.py.
+    #
+
+    if settings.BTW_TESTING and not settings.BTW_SELENIUM_TESTS:
+        for name in settings.DATABASES.keys():
+            db = settings.DATABASES[name]
+            db['NAME'] = 'test_' + db['NAME']
 
 
 app = Celery('btw')
