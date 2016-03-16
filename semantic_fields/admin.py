67ba77e5f0137950e1740196e6a172d40dc83993
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-03-16 13:08:42 -0400

    Register the administration interfaces.

--- semantic_fields/admin.py
+++ semantic_fields/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SemanticField, Lexeme, SearchWord
+
+admin.site.register(SemanticField)
+admin.site.register(Lexeme)
+admin.site.register(SearchWord)
