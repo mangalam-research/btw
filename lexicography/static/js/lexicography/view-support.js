edcf4204f379f31fc42c71fe8c4aa5946e61bdc6
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-08-15 13:50:03 -0400

    Updated to silence eslint.

--- lexicography/static/js/lexicography/view-support.js
+++ lexicography/static/js/lexicography/view-support.js
@@ -4,171 +4,184 @@
  * @author Louis-Dominique Dubeau
  */
 
-define(function (require, exports, module) {
+define(function factory(require, exports, _module) {
+  "use strict";
 
-var $ = require("jquery");
-var moment = require("moment");
-require("bootstrap-datepicker");
+  var $ = require("jquery");
+  var moment = require("moment");
+  require("bootstrap-datepicker");
 
-var body = $('#cite-modal .modal-body')[0];
-var $picker = $(document.getElementById("access-date"));
-var version_specific_checkbox = document.getElementById("version-specific");
+  var body = $("#cite-modal .modal-body")[0];
+  var $picker = $(document.getElementById("access-date"));
+  var versionSpecificCheckbox = document.getElementById("version-specific");
 
-var accessed_spans = body.getElementsByClassName("accessed");
-var url_spans = body.getElementsByClassName("url");
-var form = body.getElementsByTagName("form")[0];
-var download_mods = document.getElementById("download-mods");
-var mla_year = document.getElementById("mla_year");
-var cite_button = document.querySelector(".btn[data-target='#cite-modal']");
-var today = new Date();
+  var accessedSpans = body.getElementsByClassName("accessed");
+  var urlSpans = body.getElementsByClassName("url");
+  var form = body.getElementsByTagName("form")[0];
+  var downloadMods = document.getElementById("download-mods");
+  var mlaYear = document.getElementById("mla_year");
+  var citeButton = document.querySelector(".btn[data-target='#cite-modal']");
+  var today = new Date();
 
-$picker.datepicker({
+  $picker.datepicker({
     format: "yyyy-mm-dd",
     todayBtn: "linked",
     autoclose: true,
-    todayHighlight: true
-}).datepicker('setDate', today);
+    todayHighlight: true,
+  }).datepicker("setDate", today);
 
-mla_year.textContent = today.getFullYear();
+  mlaYear.textContent = today.getFullYear();
 
-var abs_div = document.createElement('div');
-abs_div.innerHTML = "<a></a>";
-function absolute(url) {
-    abs_div.firstChild.href = url;
-    // abs_div.innerHTML = abs_div.innerHTML;
-    return abs_div.firstChild.href;
-}
+  var absDiv = document.createElement("div");
+  absDiv.innerHTML = "<a></a>";
+  function absolute(url) {
+    absDiv.firstChild.href = url;
+    // absDiv.innerHTML = absDiv.innerHTML;
+    return absDiv.firstChild.href;
+  }
 
-return function (permalink, version_permalink, viewer) {
-
+  return function viewSupport(permalink, versionPermalink, viewer) {
     function refreshCitations() {
-        var date = $picker.datepicker('getDate');
-        var i, span;
-        for (i = 0; (span = accessed_spans[i]); ++i)
-            span.textContent = moment(date).format("MMMM D YYYY");
+      var date = $picker.datepicker("getDate");
+      var i;
+      var span;
+      for (i = 0; i < accessedSpans.length; ++i) {
+        span = accessedSpans[i];
+        span.textContent = moment(date).format("MMMM D YYYY");
+      }
 
-        var url = absolute(version_specific_checkbox.checked ? version_permalink
-                : permalink);
-        for (i = 0; (span = url_spans[i]); ++i) {
-            span.innerHTML = url;
-            span.href = url;
-        }
-
+      var url = absolute(versionSpecificCheckbox.checked ? versionPermalink
+                         : permalink);
+      for (i = 0; i < urlSpans.length; ++i) {
+        span = urlSpans[i];
+        span.innerHTML = url;
+        span.href = url;
+      }
     }
 
     $picker.on("changeDate", refreshCitations);
-    $(version_specific_checkbox).on("change", refreshCitations);
-    $(download_mods).on("click", function () {
-        form.submit();
-        return false;
+    $(versionSpecificCheckbox).on("change", refreshCitations);
+    $(downloadMods).on("click", function click() {
+      form.submit();
+      return false;
     });
 
     refreshCitations();
 
     function convertNames(els) {
-        var ret = [];
-        for (var i = 0, el; (el = els[i]); ++i) {
-            var surname = el.getElementsByClassName("surname")[0]
-                    .textContent.trim();
-            var forename = el.getElementsByClassName("forename")[0]
-                    .textContent.trim();
-            var genName = el.getElementsByClassName("genName")[0]
-                    .textContent.trim();
+      var ret = [];
+      for (var i = 0; i < els.length; ++i) {
+        var el = els[i];
+        var surname = el.getElementsByClassName("surname")[0]
+              .textContent.trim();
+        var forename = el.getElementsByClassName("forename")[0]
+              .textContent.trim();
+        var genName = el.getElementsByClassName("genName")[0]
+              .textContent.trim();
 
-            if (genName === "")
-                genName = undefined;
-
-            if (forename === "")
-                forename = undefined;
-
-            ret.push({
-                surname: surname,
-                forename: forename,
-                genName: genName
-            });
+        if (genName === "") {
+          genName = undefined;
         }
 
-        return ret;
+        if (forename === "") {
+          forename = undefined;
+        }
+
+        ret.push({
+          surname: surname,
+          forename: forename,
+          genName: genName,
+        });
+      }
+
+      return ret;
     }
 
-    function combineNames(names, reverse_first, max) {
-        var ret = "";
+    function combineNames(names, reverseFirst, max) {
+      var ret = "";
 
-        var i = 0, name = names[0];
+      var i = 0;
+      var name = names[0];
 
-        if (reverse_first) {
-            ret += name.surname;
+      if (reverseFirst) {
+        ret += name.surname;
 
-            if (name.forename)
-                ret += ", " + name.forename;
-
-            if (name.genName)
-                ret += ", " + name.genName;
-
-            if (names[1])
-                ret += (names.length > 2) ? ", " : " and ";
-
-            i++;
+        if (name.forename) {
+          ret += ", " + name.forename;
         }
 
-        // max is used for MLA format citations. If there are more
-        // than 3 authors, then we only list the first author + "et
-        // al.".
-        if (names.length > max) {
-            ret += "et al.";
-
-            return ret;
+        if (name.genName) {
+          ret += ", " + name.genName;
         }
 
-        for (; (name = names[i]); ++i) {
-            if (name.forename)
-                ret += name.forename + " ";
-
-            ret += name.surname;
-
-            if (name.genName)
-                ret += ", " + name.genName;
-
-            if (names[i + 1])
-                ret += (i < names.length - 2) ? ", " : " and ";
+        if (names[1]) {
+          ret += (names.length > 2) ? ", " : " and ";
         }
+
+        i++;
+      }
+
+      // max is used for MLA format citations. If there are more
+      // than 3 authors, then we only list the first author + "et
+      // al.".
+      if (names.length > max) {
+        ret += "et al.";
 
         return ret;
+      }
+
+      for (; i < names.length; ++i) {
+        name = names[i];
+        if (name.forename) {
+          ret += name.forename + " ";
+        }
+
+        ret += name.surname;
+
+        if (name.genName) {
+          ret += ", " + name.genName;
+        }
+
+        if (names[i + 1]) {
+          ret += (i < names.length - 2) ? ", " : " and ";
+        }
+      }
+
+      return ret;
     }
 
 
-    viewer.whenCondition("done", function () {
-        var root = viewer._root;
+    viewer.whenCondition("done", function done() {
+      var root = viewer._root;
 
-        if (!root.querySelector(".btw\\:credits")) {
-            // Leave the button disabled but add an explanatory title.
-            cite_button.setAttribute(
-                "title",
-                "This article was produced with an old version of the " +
-                    "BTW schema and thus does not support the automatic " +
-                    "generation of citations.");
-            return;
-        }
+      if (!root.querySelector(".btw\\:credits")) {
+        // Leave the button disabled but add an explanatory title.
+        citeButton.setAttribute(
+          "title",
+          "This article was produced with an old version of the " +
+            "BTW schema and thus does not support the automatic " +
+            "generation of citations.");
+        return;
+      }
 
-        // We need to wait until the document is "done" rendering so
-        // that we can grab the author information and editor
-        // information for the citations.
-        var authors = convertNames(
-            root.querySelectorAll(".btw\\:credit .persName"));
-        var editors = convertNames(root.querySelectorAll(".editor .persName"));
+      // We need to wait until the document is "done" rendering so
+      // that we can grab the author information and editor
+      // information for the citations.
+      var authors = convertNames(
+        root.querySelectorAll(".btw\\:credit .persName"));
+      var editors = convertNames(root.querySelectorAll(".editor .persName"));
 
-        var mla_authors = document.getElementById("mla_authors");
-        mla_authors.textContent = combineNames(authors, true, 3);
+      var mlaAuthors = document.getElementById("mla_authors");
+      mlaAuthors.textContent = combineNames(authors, true, 3);
 
-        var mla_editors = document.getElementById("mla_editors");
-        mla_editors.textContent = editors.length > 1 ? "Eds. " : "Ed. ";
-        mla_editors.textContent += combineNames(editors, true, 3);
+      var mlaEditors = document.getElementById("mla_editors");
+      mlaEditors.textContent = editors.length > 1 ? "Eds. " : "Ed. ";
+      mlaEditors.textContent += combineNames(editors, true, 3);
 
-        var chicago_authors = document.getElementById("chicago_authors");
-        chicago_authors.textContent = combineNames(authors);
+      var chicagoAuthors = document.getElementById("chicago_authors");
+      chicagoAuthors.textContent = combineNames(authors);
 
-        cite_button.classList.remove("disabled");
+      citeButton.classList.remove("disabled");
     });
-};
-
+  };
 });
