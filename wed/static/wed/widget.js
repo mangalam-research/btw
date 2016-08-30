d51a0f7a01737ddfc9c47f30c6444b1ee07b0efc
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-08-30 14:46:12 -0400

    Connect wed's error handler to Last Resort.

--- wed/static/wed/widget.js
+++ wed/static/wed/widget.js
@@ -14,66 +14,71 @@
   }
 
   function init() {
-    require(["wed/wed", "jquery", "jquery.cookie"], function loaded(wed, $) {
-      var widgets = document.getElementsByClassName("wed-widget");
+    require(
+      ["wed/wed", "jquery", "last-resort", "wed/onerror", "jquery.cookie"],
+      function loaded(wed, $, lr, onerror) {
 
-      for (var i = 0; i < widgets.length; i++) {
-        var widget = widgets[i];
-        var script = widget.nextElementSibling;
-        if (script.tagName !== "SCRIPT") {
-          throw new Error("script element for data not found!");
-        }
-        var $widget = $(widget);
+        var onError = lr.install(window, { force: true });
+        onError.register(onerror.handler);
 
-        /* eslint-disable camelcase, no-undef*/
-        var options = (typeof wed_config === "object") ?
-              wed_config : {};
-        /* eslint-enable camelcase, no-undef */
+        var widgets = document.getElementsByClassName("wed-widget");
 
-        var csrftoken = $.cookie("csrftoken");
-        var $parentform = $widget.parents("form").first();
+        for (var i = 0; i < widgets.length; i++) {
+          var widget = widgets[i];
+          var script = widget.nextElementSibling;
+          if (script.tagName !== "SCRIPT") {
+            throw new Error("script element for data not found!");
+          }
+          var $widget = $(widget);
 
-        options.mode = {
-          options: {
-            semanticFieldFetchUrl:
-            $parentform.find("#id_sf_fetch_url").val(),
-          },
-        };
-        options.ajaxlog = {
-          url: $parentform.find("#id_logurl").val(),
-          headers: {
-            "X-CSRFToken": csrftoken,
-          },
-        };
-        options.save = {
-          path: "wed/savers/ajax",
-          options: {
-            url: $parentform.find("#id_saveurl").val(),
+          /* eslint-disable camelcase, no-undef*/
+          var options = (typeof wed_config === "object") ? wed_config : {};
+          /* eslint-enable camelcase, no-undef */
+
+          var csrftoken = $.cookie("csrftoken");
+          var $parentform = $widget.parents("form").first();
+
+          options.mode = {
+            options: {
+              semanticFieldFetchUrl:
+              $parentform.find("#id_sf_fetch_url").val(),
+            },
+          };
+          options.ajaxlog = {
+            url: $parentform.find("#id_logurl").val(),
             headers: {
               "X-CSRFToken": csrftoken,
             },
-            initial_etag: $parentform.find("#id_initial_etag").val(),
-          },
-        };
+          };
+          options.save = {
+            path: "wed/savers/ajax",
+            options: {
+              url: $parentform.find("#id_saveurl").val(),
+              headers: {
+                "X-CSRFToken": csrftoken,
+              },
+              initial_etag: $parentform.find("#id_initial_etag").val(),
+            },
+          };
 
-        // eslint-disable-next-line camelcase
-        var wed_editor = new wed.Editor();
-        wed_editor.init(widget, options, script.textContent);
-        // Yep, this means only one wed editor per window.
-        // eslint-disable-next-line camelcase
-        window.wed_editor = wed_editor;
-        wed_editor.whenCondition("initialized",
-                                 // eslint-disable-next-line no-loop-func
-                                 function initialized() {
-                                   $widget.prev().remove();
+          // eslint-disable-next-line camelcase
+          var wed_editor = new wed.Editor();
+          wed_editor.init(widget, options, script.textContent);
+          // Yep, this means only one wed editor per window.
+          // eslint-disable-next-line camelcase
+          window.wed_editor = wed_editor;
+          wed_editor.whenCondition("initialized",
+                                   // eslint-disable-next-line no-loop-func
+                                   function initialized() {
+                                     $widget.prev().remove();
 
-                                   // Allow CSS to reflow
-                                   window.setTimeout(
-                                     wed_editor.resize.bind(wed_editor),
-                                     0);
-                                 });
-      }
-    });
+                                     // Allow CSS to reflow
+                                     window.setTimeout(
+                                       wed_editor.resize.bind(wed_editor),
+                                       0);
+                                   });
+        }
+      });
   }
 
   if (window.addEventListener) { // DOM
