dcd2d7df2fa8939c71bbee50d61ec766d0ad3204
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-09-22 12:50:29 -0400

    Work around a compatibility issue.
    Marionette 3 and Backbone Relational have a problem working
    together. See:
    
    https://github.com/PaulUithol/Backbone-relational/issues/565

--- static-src/lib/marionette-glue.js
+++ static-src/lib/marionette-glue.js
@@ -4,7 +4,22 @@ define(function factory(require) {
   var Mn = require("marionette");
   var Bb = require("backbone");
 
+  Mn.View.prototype.serializeModel = function serializeModel() {
+    if (!this.model) {
+      return {};
+    }
+
+    return this.model.toJSON();
   };
+
+  Mn.View.prototype.serializeCollection = function serializeCollection() {
+    if (!this.collection) {
+      return {};
+    }
+
+    return this.collection.toJSON();
+  };
+
   if (window.__agent) {
     window.__agent.start(Bb, Mn);
   }
