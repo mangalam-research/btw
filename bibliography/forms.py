4a460cd61d9c7a8d3e02f4bd0a1f1ac1cc3962e9
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-10-31 10:56:48 -0400

    Set strip=False for Django 1.10.
    Django 1.10 introduced ``strip=True`` by default. It defeats our own checks.

--- bibliography/forms.py
+++ bibliography/forms.py
@@ -3,11 +3,12 @@ from .models import PrimarySource
 
 
 class PrimarySourceForm(forms.ModelForm):
+    reference_title = forms.CharField(strip=False,
+                                      widget=forms.Textarea(attrs={'rows': 2}))
 
     class Meta(object):
         model = PrimarySource
         fields = ('item', 'reference_title', 'genre')
         widgets = {
             'item': forms.HiddenInput(),
-            'reference_title': forms.Textarea(attrs={'rows': 2})
         }
