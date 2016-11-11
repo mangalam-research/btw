297f35c0133558a2be698fffca6a516643785dd1
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-11-11 08:30:59 -0500

    Satisfy the linter.

--- semantic_fields/static/js/semantic-field-search.js
+++ semantic_fields/static/js/semantic-field-search.js
@@ -1,84 +1,59 @@
-define(['jquery', './displayers', 'jquery.cookie', 'datatables.bootstrap'],
-       function ($, displayers) {
+define(function factory(require) {
+  "use strict";
 
-var Displayers = displayers.Displayers;
-var Displayer = displayers.Displayer;
+  var $ = require("jquery");
+  var Displayers = require("./displayers").Displayers;
+  require("jquery.cookie");
+  require("datatables.bootstrap");
 
-// This number should be bumped whenever we must flush the old saved
-// state but do not have any other means to do so.
-var _LOCAL_VERSION=1;
+  // This number should be bumped whenever we must flush the old saved
+  // state but do not have any other means to do so.
+  var _LOCAL_VERSION = 1;
 
-/**
- * Manages a semantic field search table. This means:
- *
- * - Create the DataTables object which will link the HTML table to
- *   the Django backend. Customize the table to display the right
- *   information and send the right parameters to the Django backend.
- *
- * - Display semantic field details when a user clicks on a semantic
- *   field shown by the table.
- *
- * - Manage each semantic field display. Using {@link
- *   module:semantic-field-search~Displayers Displayers} and {@link
- *   module:semanatic-field-search~Displayer Displayer}
- */
-return function ($table, display_div, options) {
-    var doc = display_div.ownerDocument;
-    var displayers = new Displayers(display_div);
+  /**
+   * Manages a semantic field search table. This means:
+   *
+   * - Create the DataTables object which will link the HTML table to
+   *   the Django backend. Customize the table to display the right
+   *   information and send the right parameters to the Django backend.
+   *
+   * - Display semantic field details when a user clicks on a semantic
+   *   field shown by the table.
+   *
+   * - Manage each semantic field display. Using {@link
+   *   module:semantic-field-search~Displayers Displayers} and {@link
+   *   module:semanatic-field-search~Displayer Displayer}
+   */
+  return function semanticFieldSearch($table, displayDiv, options) {
+    var doc = displayDiv.ownerDocument;
+    var displayers = new Displayers(displayDiv);
 
-    var template = '\
- <i class="fa fa-question-circle text-info"></i> <label>in \
-<select class="form-control input-sm" name="aspect">\
-<option value="sf">semantic field names</option>\
-<option value="lexemes">lexemes</option>\
+    var template = "\
+ <i class='fa fa-question-circle text-info'></i> <label>in \
+<select class='form-control input-sm' name='aspect'>\
+<option value='sf'>semantic field names</option>\
+<option value='lexemes'>lexemes</option>\
 </select></label>\
- <i class="fa fa-question-circle text-info"></i> <label>among \
-<select class="form-control input-sm" name="scope">\
-<option value="all">all fields</option>\
-<option value="hte">HTE fields</option>\
-<option value="btw">custom BTW fields</option>\
+ <i class='fa fa-question-circle text-info'></i> <label>among \
+<select class='form-control input-sm' name='scope'>\
+<option value='all'>all fields</option>\
+<option value='hte'>HTE fields</option>\
+<option value='btw'>custom BTW fields</option>\
 </select></label>\
- <i class="fa fa-question-circle text-info"></i>\
-';
+ <i class='fa fa-question-circle text-info'></i>\
+";
 
-    var search_aspect = null;
-    var search_scope = null;
+    var searchAspect = null;
+    var searchScope = null;
 
     function makeHelpPopover(el, _class) {
-        $(el).popover({
-            placement: "auto",
-            content: doc.querySelector(
-                ".semantic-field-search-help ." + _class),
-            html: "true"
-        });
+      $(el).popover({
+        placement: "auto",
+        content: doc.querySelector(
+          ".semantic-field-search-help ." + _class),
+        html: "true",
+      });
     }
-
-    function drawCallback() {
-        if (!search_aspect || !search_aspect.parentNode) {
-            var filter =
-                    doc.getElementById("semantic-field-table_wrapper")
-                    .getElementsByClassName("dataTables_filter")[0];
-            var input = filter.getElementsByTagName("input")[0];
-            input.parentNode.insertAdjacentHTML("afterend", template);
-            search_aspect = filter.querySelector("select[name='aspect']");
-            search_scope = filter.querySelector("select[name='scope']");
-
-            var help_els = filter
-                    .getElementsByClassName('fa-question-circle');
-            makeHelpPopover(help_els[0], "search-help");
-            makeHelpPopover(help_els[1], "aspect-help");
-            makeHelpPopover(help_els[2], "scope-help");
-
-            GUIFromState();
-            $([search_aspect, search_scope]).change(function () {
-                StateFromGUI();
-                table.api().draw();
-            });
-        }
-
-    }
-
-    var token = _LOCAL_VERSION;
 
     // We maintain a state-of-the-GUI due to the way DataTables
     // operates. It is possible for the first ajax query to happen
@@ -88,108 +63,134 @@ return function ($table, display_div, options) {
     var state;
 
     function resetState() {
-        state = {
-            aspect: "sf",
-            scope: "all"
-        };
+      state = {
+        aspect: "sf",
+        scope: "all",
+      };
     }
+
+    function guiFromState() {
+      var aspect = state.aspect;
+      if (searchAspect && aspect) {
+        searchAspect.value = aspect;
+      }
+
+      var scope = state.scope;
+      if (searchScope && scope) {
+        searchScope.value = scope;
+      }
+    }
+
+    function stateFromGUI() {
+      if (searchAspect) {
+        state.aspect = searchAspect.value;
+      }
+
+      if (searchScope) {
+        state.scope = searchScope.value;
+      }
+    }
+
+    function drawCallback() {
+      if (!searchAspect || !searchAspect.parentNode) {
+        var filter =
+              doc.getElementById("semantic-field-table_wrapper")
+              .getElementsByClassName("dataTables_filter")[0];
+        var input = filter.getElementsByTagName("input")[0];
+        input.parentNode.insertAdjacentHTML("afterend", template);
+        searchAspect = filter.querySelector("select[name='aspect']");
+        searchScope = filter.querySelector("select[name='scope']");
+
+        var helpEls = filter
+              .getElementsByClassName("fa-question-circle");
+        makeHelpPopover(helpEls[0], "search-help");
+        makeHelpPopover(helpEls[1], "aspect-help");
+        makeHelpPopover(helpEls[2], "scope-help");
+
+        guiFromState();
+        $([searchAspect, searchScope]).change(function onChange() {
+          stateFromGUI();
+          table.api().draw(); // eslint-disable-line no-use-before-define
+        });
+      }
+    }
+
+    var token = _LOCAL_VERSION;
 
     resetState();
 
-    function GUIFromState() {
-        var aspect = state.aspect;
-        if (search_aspect && aspect) {
-            search_aspect.value = aspect;
-        }
+    $.fn.dataTable.ext.errMode = "none";
 
-        var scope = state.scope;
-        if (search_scope && scope) {
-            search_scope.value = scope;
-        }
-    }
-
-    function StateFromGUI() {
-        if (search_aspect) {
-            state.aspect = search_aspect.value;
-        }
-
-        if (search_scope) {
-            state.scope = search_scope.value;
-        }
-    }
-
-    $.fn.dataTable.ext.errMode = 'none';
-
-    $table.on("error.dt", function () {
-        resetState();
-        StateFromGUI();
-        $(this).DataTable().state.clear();
+    $table.on("error.dt", function onError() {
+      resetState();
+      stateFromGUI();
+      $(this).DataTable().state.clear(); // eslint-disable-line new-cap
     });
 
     var table = $table.dataTable({
-        dom: "<'row'<'col-sm-2'l><'col-sm-10'f>>" +
-	    "<'row'<'col-sm-12'tr>>" +
-	    "<'row'<'col-sm-5'i><'col-sm-7'p>>",
-        processing: true,
-        serverSide: true,
-        autoWidth: false,
-        stateSave: true,
-        order: [[1, 'asc']],
-        stateSaveParams: function (settings, data) {
-            data.btw_token = token;
-            data.aspect = state.aspect;
-            data.scope = state.scope;
-        },
-        stateLoadParams: function (settings, data) {
-            if (data.btw_token !== token) {
-                // The token changed, clear the state.
-                resetState();
-                this.api().state.clear();
-                return false;
-            }
-            return undefined;
-        },
-        stateLoaded: function (settings, data) {
-            var aspect = data.aspect;
-            if (aspect && !/^[a-z]+$/.test(aspect))
-                data.aspect = undefined; // Corrupt data, reset.
+      dom: "<'row'<'col-sm-2'l><'col-sm-10'f>>" +
+        "<'row'<'col-sm-12'tr>>" +
+        "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+      processing: true,
+      serverSide: true,
+      autoWidth: false,
+      stateSave: true,
+      order: [[1, "asc"]],
+      stateSaveParams: function stateSaveParams(settings, data) {
+        data.btw_token = token;
+        data.aspect = state.aspect;
+        data.scope = state.scope;
+      },
+      stateLoadParams: function stateLoadParams(settings, data) {
+        if (data.btw_token !== token) {
+          // The token changed, clear the state.
+          resetState();
+          this.api().state.clear();
+          return false;
+        }
+        return undefined;
+      },
+      stateLoaded: function stateLoaded(settings, data) {
+        var aspect = data.aspect;
+        if (aspect && !/^[a-z]+$/.test(aspect)) {
+          data.aspect = undefined; // Corrupt data, reset.
+        }
 
-            var scope = data.scope;
-            if (scope && !/^[a-z]+$/.test(scope))
-                data.scope = undefined; // Corrupt data, reset.
+        var scope = data.scope;
+        if (scope && !/^[a-z]+$/.test(scope)) {
+          data.scope = undefined; // Corrupt data, reset.
+        }
 
-            state = data;
-            GUIFromState();
+        state = data;
+        guiFromState();
+      },
+      ajax: {
+        url: options.ajax_source_url,
+        data: function data(params) {
+          params.aspect = state.aspect;
+          params.scope = state.scope;
         },
-        ajax: {
-            url: options.ajax_source_url,
-            data: function (params) {
-                params.aspect = state.aspect;
-                params.scope = state.scope;
-            }
+      },
+      columnDefs: [{
+        targets: [0],
+        orderable: false,
+        visible: false,
+      }, {
+        targets: [1],
+        createdCell: function createdCell(cell) {
+          $(cell.getElementsByClassName("sf-link")).click(function onClick(ev) {
+            displayers.open(ev.target.href);
+            return false;
+          });
         },
-        columnDefs: [{
-            targets: [0],
-            orderable: false,
-            visible: false
-        }, {
-            targets: [1],
-            createdCell: function (cell, data, row_data, row_ix, cell_ix) {
-                $(cell.getElementsByClassName("sf-link")).click(function (ev) {
-                    displayers.open(ev.target.href);
-                    return false;
-                });
-
-            }
-        }],
-        drawCallback: drawCallback
+      }],
+      drawCallback: drawCallback,
     });
 
     $table.data("search-table", table);
     return {
-        table: table,
-        displayers: displayers
+      table: table,
+      displayers: displayers,
     };
-};
-
+  };
 });
