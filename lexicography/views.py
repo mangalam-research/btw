d7ae22bfa4a8059571eda864894704f149d6f0ad
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-11-28 09:49:08 -0500

    Require latest wed.

--- lexicography/views.py
+++ lexicography/views.py
@@ -599,7 +599,7 @@ def handle_update(request, handle_or_entry_id):
 # opportunity to change. The Django **code** depends on wed being at a
 # certain version. Changing this in the settings file would be a
 # recipe for disaster.
-REQUIRED_WED_VERSION = "0.27.0"
+REQUIRED_WED_VERSION = "0.28.2"
 
 
 def version_check(version):
