537e4a4f1cf1cf6a30f750e04e8f9daf2bedaf0c
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-09-06 13:28:22 -0400

    Add a patch to work around a DRF serialization failure.

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,43 @@
 from lib import env
+import rest_framework
+from rest_framework.response import Response
+
+#
+# This file contains patches to Django and libraries in order to work
+# around issues. Usually, we don't want patches but sometimes the
+# pragmatic solution rather than wait for a fix in the official code
+# (which may take a while) is to monkey patch and move on.
+#
+# Note that btw/celery.py also contains patches that are proper to
+# running in celery.
+#
+# runner.py contains patches proper to running the test suite but that
+# are not used in production.
+#
+
+#
+# This DRF patch drops the data field from the serialized data of
+# response objects. See
+# https://github.com/encode/django-rest-framework/issues/5396
+#
+drf_version = tuple(int(x) for x in rest_framework.VERSION.split("."))
+
+if drf_version > (3, 6, 4):
+    raise Exception("check whether the DRF patch is still needed with "
+                    "this version of DRF.")
+
+if drf_version <= (3, 6, 4):
+    oldGetState = Response.__getstate__
+
+    def newGetState(self):
+        state = oldGetState(self)
+        for key in ('data', ):
+            if key in state:
+                del state[key]
+        return state
+
+    Response.__getstate__ = newGetState
+
 if env.testing:
     #
     # We modify the connections so that the code cannot access the
