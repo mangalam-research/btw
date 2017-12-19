d23e71fcb055bc35edd80e4958e35a9c5cc425f3
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-12-19 14:54:41 -0500

    Lint.

--- bibliography/static/scripts/refresh.js
+++ bibliography/static/scripts/refresh.js
@@ -1,76 +1,77 @@
-define(["jquery", "jquery.cookie", "jquery.bootstrap-growl"], function ($) {
+define(["jquery", "jquery.cookie", "jquery.bootstrap-growl"], function r($) {
+  "use strict";
 
-return function(initiate_url, check_url) {
+  return function refresh(initiateUrl, checkUrl) {
     var span = document.getElementById("btw-prev-refreshed");
-    var table_el = document.getElementById("bibliography-table");
+    var tableEl = document.getElementById("bibliography-table");
     var button = document.getElementById("btw-refresh");
     var icon = button.getElementsByTagName("i")[0];
-    var csrftoken = $.cookie('csrftoken');
+    var csrftoken = $.cookie("csrftoken");
 
     function working() {
-        icon.classList.add("fa-spin");
-        button.disabled = true;
+      icon.classList.add("fa-spin");
+      button.disabled = true;
     }
 
     function idle() {
-        icon.classList.remove("fa-spin");
-        button.disabled = false;
+      icon.classList.remove("fa-spin");
+      button.disabled = false;
     }
 
     function growl(message, type) {
-        $.bootstrapGrowl(message, {
-            ele: 'body',
-            type: type,
-            offset: {from: 'top', amount: 20},
-            align: 'center',
-            width: 'auto',
-            delay: 4000,
-            allow_dismiss: true,
-            stackup_spacing: 10
-        });
+      $.bootstrapGrowl(message, {
+        ele: "body",
+        type: type,
+        offset: { from: "top", amount: 20 },
+        align: "center",
+        width: "auto",
+        delay: 4000,
+        allow_dismiss: true,
+        stackup_spacing: 10,
+      });
     }
 
-    function failed(jqXHR) {
-        idle();
-        growl("Refresh failed. Please contact technical support.", "danger");
+    function failed() {
+      idle();
+      growl("Refresh failed. Please contact technical support.", "danger");
     }
 
-    $(button).on("click", function (ev) {
-        working();
-        $.ajax({
-            url: initiate_url,
-            type: "POST",
+    $(button).on("click", function click() {
+      working();
+      $.ajax({
+        url: initiateUrl,
+        type: "POST",
+        headers: {
+          "X-CSRFToken": csrftoken,
+          Accept: "application/json",
+        },
+      }).done(function done(data) {
+        var last = data;
+        function check() {
+          $.ajax({
+            url: checkUrl,
+            type: "GET",
             headers: {
-                'X-CSRFToken': csrftoken,
-                Accept: "application/json"
+              Accept: "application/json",
+            },
+          }).done(function doneCheck(checkData) {
+            var now = checkData;
+            if (now === last) {
+              setTimeout(check, 500);
             }
-        }).done(function (data) {
-            var last = data;
-            function check () {
-                $.ajax({
-                    url: check_url,
-                    type: "GET",
-                    headers: {
-                        Accept: "application/json"
-                    }
-                }).done(function (data) {
-                    var now = data;
-                    if (now === last)
-                        setTimeout(check, 500);
-                    else {
-                        idle();
-                        span.textContent = data;
-                        var table = $.data(table_el, "title-table");
-                        if (table)
-                            table.fnDraw();
-                        growl("Refreshed!", "success");
-                    }
-
-                }).fail(failed);
+            else {
+              idle();
+              span.textContent = checkData;
+              var table = $.data(tableEl, "title-table");
+              if (table) {
+                table.fnDraw();
+              }
+              growl("Refreshed!", "success");
             }
-            check();
-        }).fail(failed);
+          }).fail(failed);
+        }
+        check();
+      }).fail(failed);
     });
-};
-
+  };
 });
