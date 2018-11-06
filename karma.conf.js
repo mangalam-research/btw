d603a0e01e5e9c5b7a464cf5d86a54e206a78614
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-11-06 11:07:31 -0500

    Use a headless Chrome for testing.

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function configure(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["ChromeHeadless"],
     singleRun: false,
   });
 };
