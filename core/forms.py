9f23dc2056c7cb8cf9f423d92035559875ee04ad
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-04-08 15:27:39 -0400

    The save method is deprecated in favor of signup.

--- core/forms.py
+++ core/forms.py
@@ -10,7 +10,7 @@ class SignupForm(forms.Form):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
 
-    def save(self, user):
+    def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.save()
