88c5a2b0134e1ba954b764e6c3a2d0b282e8e2fb
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-05-01 13:51:50 -0400

    Drop roma in favor of the newer TEI tools.

--- utils/schemas/include.mk
+++ utils/schemas/include.mk
@@ -4,17 +4,20 @@
 #
 ME:=UTIL_SCHEMAS
 
-$(ME)_include_mk_DIR=$(patsubst %/,%,$(or $(dir $(lastword $(MAKEFILE_LIST))),.))
+$(ME)_include_mk_DIR:=$(patsubst %/,%,$(or $(dir $(lastword $(MAKEFILE_LIST))),.))
 $(ME)_outdir:=$($(ME)_include_mk_DIR)/out
 
 # Top of tei hierarchy
 $(ME)_TEI?=$(TEI)
-$(ME)_CONVERT?=$(WED_PATH)/node_modules/.bin/salve-convert
 $(ME)_ODD2HTML?=$($(ME)_TEI)/odds/odd2html.xsl
 $(ME)_SAXON?=saxon
-$(ME)_ROMA?=utils/roma
+$(ME)_TEITOODD?=teitoodd
+$(ME)_TEITORNC?=teitornc
+$(ME)_TRANG?=trang
 # Parameters common to all invocations of Roma.
-$(ME)_ROMA_PARAMS?=--remoteversion=2.9.1 --xsl=/usr/share/xml/tei/stylesheet
+$(ME)_LOCALSOURCE?=--localsource=$($(ME)_include_mk_DIR)/p5subset.xml
+$(ME)_TEITORNC_PARAMS?=$($(ME)_LOCALSOURCE)
+$(ME)_TEITOODD_PARAMS?=$($(ME)_LOCALSOURCE)
 
 # ls -rv sorts in reverse order by version number. (Yep, ls can do this!)
 $(ME)_VERSIONS:=$(shell ls -rv $($(ME)_include_mk_DIR)/btw-storage-*.xml)
@@ -31,26 +34,23 @@ $(ME)_LATEST_XSL_TARGET:=$(firstword $($(ME)_XSL_TARGETS))
 # We need only the latest for these because we only edit the latest version.
 $(ME)_LATEST_METADATA_TARGET:=$($(ME)_LATEST_RNG_TARGET:.rng=-metadata.json)
 $(ME)_LATEST_DOC_TARGET:=$($(ME)_LATEST_RNG_TARGET:.rng=-doc)
-$(ME)_LATEST_JS_TARGET:=$($(ME)_LATEST_RNG_TARGET:.rng=.js)
 
 $(ME)_FLAT_MODS:=$($(ME)_outdir)/flat_mods/mods-3-5.xsd
 
 .PHONY: btw-schema-targets
-btw-schema-targets: $($(ME)_RNG_TARGETS) $($(ME)_LATEST_DOC_TARGET) $($(ME)_LATEST_METADATA_TARGET) $($(ME)_LATEST_JS_TARGET) $($(ME)_XSL_TARGETS) $($(ME)_FLAT_MODS)
+btw-schema-targets: $($(ME)_RNG_TARGETS) $($(ME)_LATEST_DOC_TARGET) $($(ME)_LATEST_METADATA_TARGET) $($(ME)_XSL_TARGETS) $($(ME)_FLAT_MODS)
 
 $($(ME)_outdir)/btw-storage-latest.rng: $($(ME)_LATEST_RNG_TARGET)
 $($(ME)_outdir)/btw-storage-metadata-latest.json: $($(ME)_LATEST_METADATA_TARGET)
 $($(ME)_outdir)/btw-storage-doc-latest: $($(ME)_LATEST_DOC_TARGET)
-$($(ME)_outdir)/btw-storage-latest.js: $($(ME)_LATEST_JS_TARGET)
-
 
 $($(ME)_outdir)/btw-storage-latest.rng $($(ME)_outdir)/btw-storage-metadata-latest.json $($(ME)_outdir)/btw-storage-doc-latest $($(ME)_outdir)/btw-storage-latest.js:
 	rm -rf $@
 	cp -rp $< $@
 
 $($(ME)_RNG_TARGETS): $($(ME)_outdir)/%/btw-storage.rng: $($(ME)_include_mk_DIR)/%.xml
-	$($(ME)_ROMA) $($(ME)_ROMA_PARAMS) --dochtml --noxsd --nodtd \
-		$< $(dir $@)
+	$($(ME)_TEITORNC) $($(ME)_TEITORNC_PARAMS) $< $(@:.rng=.rnc)
+	$($(ME)_TRANG) $(@:.rng=.rnc) $@
 
 $($(ME)_XSL_TARGETS): $($(ME)_outdir)/%.xsl: $($(ME)_include_mk_DIR)/%.sch
 	saxon -s:$< -o:$@ -xsl:$(SCHEMATRON_TO_XSL) allow-foreign=true generate-fired-rule=false
@@ -59,7 +59,7 @@ $($(ME)_XSL_TARGETS): $($(ME)_outdir)/%.xsl: $($(ME)_include_mk_DIR)/%.sch
 $(ME)_MAKE_COMPILED_NAME=$($(ME)_outdir)/$(1)/btw-storage.compiled
 define $(ME)_MAKE_COMPILED_RULE
 $$(call $(ME)_MAKE_COMPILED_NAME,$(1)): $$($(ME)_include_mk_DIR)/$(1).xml
-	$$($(ME)_ROMA) $($(ME)_ROMA_PARAMS) --compile $$< $($(ME)_outdir)/$(1)
+	$($(ME)_TEITOODD) $($(ME)_TEITOODD_PARAMS) $$< $$(call $(ME)_MAKE_COMPILED_NAME,$(1))
 endef # MAKE_COMPILED_RULE
 
 $(foreach t,$($(ME)_VERSIONS),$(eval $(call $(ME)_MAKE_COMPILED_RULE,$(t:$($(ME)_include_mk_DIR)/%.xml=%))))
@@ -69,9 +69,6 @@ $($(ME)_LATEST_RNG_TARGET:.rng=.json): $($(ME)_outdir)/%/btw-storage.json: $$(ca
 
 $($(ME)_LATEST_METADATA_TARGET): $($(ME)_outdir)/%/btw-storage-metadata.json: $($(ME)_outdir)/%/btw-storage.json $($(ME)_include_mk_DIR)/btw-storage-metadata-fragment.yml $($(ME)_outdir)/%/btw-storage-doc
 	$(WED_PATH)/bin/wed-metadata --tei --merge $(word 2,$^) $< $@
-
-$($(ME)_outdir)/%/btw-storage.js: $($(ME)_outdir)/%/btw-storage.rng
-	$($(ME)_CONVERT) $($(ME)_RNG_TO_JS) $< $@
 
 $($(ME)_outdir)/%/btw-storage-doc: $$(call $(ME)_MAKE_COMPILED_NAME,%)
 	-rm -rf $@
